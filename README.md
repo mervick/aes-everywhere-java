3f971deae21dce40781641d56c050108c35ae4df
Andrey Izman <izmanw@gmail.com>
2019-09-24 04:08:40 +0300

    Move java version from main repo to aes-everywhere-java

---
+++ README.md
@@ -0,0 +1,87 @@
+# AES Everywhere - Cross Language Encryption Library
+
+[AES Everywhere]() is Cross Language Encryption Library which provides the ability to encrypt and decrypt data using a single algorithm in different programming languages and on different platforms.
+
+This is an implementation of the AES algorithm, specifically CBC mode, with 256 bits key length and PKCS7 padding.
+It implements OpenSSL compatible cryptography with random generated salt
+
+
+## [Java](https://www.java.com) implementation
+
+
+### Installation
+
+Via [maven](https://maven.apache.org/), add in dependencies of your `pom.xml`:
+
+```xml
+<dependencies>
+  <dependency>
+    <groupId>com.github.mervick</groupId>
+    <artifactId>aes-everywhere</artifactId>
+    <version>1.2.7</version>
+  </dependency>
+</dependencies>
+```
+
+Via [gradle](https://gradle.org/), add in dependencies of your `build.gradle`:
+```
+dependencies {
+  implementation 'com.github.mervick:aes-everywhere:1.2.7'
+}
+```
+
+Building from the sources:
+
+```bash
+mvn package 
+```
+
+
+### Import Aes256 in your project
+
+
+**Java &gt;= 8**
+
+```java
+import com.github.mervick.aes_everywhere.Aes256;
+
+```
+
+**Android with API level &lt; 26 or Java &lt; 8**  
+(also in case if you get error about java.util.Base64)
+
+```java
+import com.github.mervick.aes_everywhere.legacy.Aes256;
+```
+
+### Usage
+
+```java
+String text = "TEXT";
+String pass = "PASSWORD";
+
+byte[] text_bytes = text.getBytes("UTF-8");
+byte[] pass_bytes = pass.getBytes("UTF-8");
+
+// strings encryption
+String encrypted = Aes256.encrypt(text, pass);
+System.out.println(encrypted);
+
+// bytes encryption
+byte[] encrypted_bytes = Aes256.encrypt(text_bytes, pass_bytes);
+System.out.println(encrypted_bytes);
+
+// strings decryption
+String decrypted = Aes256.decrypt(encrypted, pass);
+System.out.println(decrypted);
+
+// bytes decryption
+byte[] decrypted_bytes = Aes256.decrypt(encrypted_bytes, pass_bytes);
+System.out.println(decrypted_bytes);
+```
+
+## Known issues
+
+[java.security.InvalidKeyException: Illegal key size](https://github.com/mervick/aes-everywhere/issues/5)
+
+This exception throws because of a restriction imposed by the default JDK. On the default JDK installation AES is limited to 128 bit key size.
---
+++ src/com/github/mervick/aes_everywhere/legacy/Aes256.java
@@ -0,0 +1,78 @@
+// Aes256.java
+// This file is part of AES-everywhere project (https://github.com/mervick/aes-everywhere)
+//
+// This is an implementation of the AES algorithm, specifically CBC mode,
+// with 256 bits key length and PKCS7 padding.
+//
+// Copyright Andrey Izman (c) 2018-2019 <izmanw@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package com.github.mervick.aes_everywhere.legacy;
+
+import com.github.mervick.aes_everywhere.AbstractAes256;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+
+public class Aes256 extends AbstractAes256
+{
+    /**
+     * Encrypt text with the passphrase
+     * @param input Input text to encrypt
+     * @param passphrase The passphrase
+     * @return A base64 encoded string containing the encrypted data
+     * @throws Exception Throws exceptions
+     */
+    public static String encrypt(String input, String passphrase) throws Exception {
+        return Base64.encodeToString(_encrypt(input.getBytes(UTF_8), passphrase.getBytes(UTF_8)), Base64.NO_WRAP);
+    }
+
+    /**
+     * Encrypt text in bytes with the passphrase
+     * @param input Input data in bytes to encrypt
+     * @param passphrase The passphrase in bytes
+     * @return A base64 encoded bytes containing the encrypted data
+     * @throws Exception Throws exceptions
+     */
+    public static byte[] encrypt(byte[] input, byte[] passphrase) throws Exception {
+        return Base64.encode(_encrypt(input, passphrase), Base64.NO_WRAP);
+    }
+
+    /**
+     * Decrypt encrypted base64 encoded text in bytes
+     * @param crypted Text in bytes to decrypt
+     * @param passphrase The passphrase in bytes
+     * @return Decrypted data in bytes
+     * @throws Exception Throws exceptions
+     */
+    public static String decrypt(String crypted, String passphrase) throws Exception {
+        return new String(_decrypt(Base64.decode(crypted, Base64.NO_WRAP), passphrase.getBytes(UTF_8)), UTF_8);
+    }
+
+    /**
+     * Decrypt encrypted base64 encoded text in bytes
+     * @param crypted Text in bytes to decrypt
+     * @param passphrase The passphrase in bytes
+     * @return Decrypted data in bytes
+     * @throws Exception Throws exceptions
+     */
+    public static byte[] decrypt(byte[] crypted, byte[] passphrase) throws Exception {
+        return _decrypt(Base64.decode(crypted, Base64.NO_WRAP), passphrase);
+    }
+}
